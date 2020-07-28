941bcc58bf2fc1ea06149fadefa3f4f10ffde2b6
Rubylium <alexisoko0@gmail.com>
2020-07-28 17:20:32 +0200

    Add files via upload

---
+++ RageUI/components/Visual.lua
@@ -0,0 +1,23 @@
+---
+--- @author Dylan MALANDAIN
+--- @version 2.0.0
+--- @since 2020
+---
+--- RageUI Is Advanced UI Libs in LUA for make beautiful interface like RockStar GAME.
+---
+---
+--- Commercial Info.
+--- Any use for commercial purposes is strictly prohibited and will be punished.
+---
+--- @see RageUI
+---
+
+
+-- TODO Rework visual wrapper
+
+---@class Visual
+Visual = Visual or {};
+
+function Visual.Popup()
+
+end
